ced1c8ae4d785149da089e616bc1fea88776ec6c
xususan <susan.xu97@gmail.com>
2019-03-22 23:46:28 -0400

    initial commit to fork

--- demo.sh
+++ demo.sh
@@ -5,17 +5,18 @@ set -e
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
 
 make
-if [ ! -e text8 ]; then
-  if hash wget 2>/dev/null; then
-    wget http://mattmahoney.net/dc/text8.zip
-  else
-    curl -O http://mattmahoney.net/dc/text8.zip
-  fi
-  unzip text8.zip
-  rm text8.zip
-fi
+# if [ ! -e text8 ]; then
+#   if hash wget 2>/dev/null; then
+#     wget http://mattmahoney.net/dc/text8.zip
+#   else
+#     curl -O http://mattmahoney.net/dc/text8.zip
+#   fi
+#   unzip text8.zip
+#   rm text8.zip
+# fi
 
-CORPUS=text8
+# CORPUS=train_2m_glove.csv
+CORPUS=train_200k_glove.csv
 VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
@@ -23,8 +24,8 @@ BUILDDIR=build
 SAVE_FILE=vectors
 VERBOSE=2
 MEMORY=4.0
-VOCAB_MIN_COUNT=5
-VECTOR_SIZE=50
+VOCAB_MIN_COUNT=1
+VECTOR_SIZE=512
 MAX_ITER=15
 WINDOW_SIZE=15
 BINARY=2
@@ -42,7 +43,7 @@ echo "$ $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file 
 $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
 if [ "$CORPUS" = 'text8' ]; then
    if [ "$1" = 'matlab' ]; then
-       matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2 
+       matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2
    elif [ "$1" = 'octave' ]; then
        octave < ./eval/octave/read_and_evaluate_octave.m 1>&2
    else
